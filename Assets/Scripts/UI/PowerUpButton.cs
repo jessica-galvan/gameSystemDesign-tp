c467ebac7b8768b6ede261d5abf2291f95b70166
Jessica <49495673+jessica-galvan@users.noreply.github.com>
2023-10-22 19:29:37 -0300

    LevelUp UI on going

--- Assets/Scripts/UI/PowerUpButton.cs
+++ Assets/Scripts/UI/PowerUpButton.cs
@@ -8,10 +8,20 @@ using UnityEngine.UI;
 
 public class PowerUpButton : BaseButton
 {
-    [SerializeField] private Image image;
+    [SerializeField] private Image imgIcon;
     [SerializeField] private TMP_Text txtDescription;
+    [SerializeField] private Image imgBorder;
+    [SerializeField] private Color selectedColor = Color.white;
+
+    private Color originalBorderColor;
 
     public ISelectableOption CurrentOption { get; private set; }
+
+    public override void Initialize()
+    {
+        originalBorderColor = imgBorder.color;
+        base.Initialize();
+    }
 
     public void SetSelectableOption(ISelectableOption option)
     {
@@ -20,7 +30,21 @@ public class PowerUpButton : BaseButton
         txtTitle.SetText(CurrentOption.Title);
         txtDescription.SetText(CurrentOption.Description);
 
-        if(image != null)
-            image.sprite = CurrentOption.Icon;
+        if(imgIcon != null)
+            imgIcon.sprite = CurrentOption.Icon;
+    }
+
+    public override void Select()
+    {
+        base.Select();
+        if (imgBorder != null)
+            imgBorder.color = selectedColor;
+    }
+
+    public override void Deselect()
+    {
+        base.Deselect();
+        if (imgBorder != null)
+            imgBorder.color = originalBorderColor;
     }
 }
